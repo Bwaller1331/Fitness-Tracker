eaaa616c4a2a72642b4f3fc50390e79aaa84383e
Brandon Waller <bwall1331@gmail.com>
2021-06-16 13:56:04 -0400

    add process.env to server

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
- 
-const PORT = 3000;
+
 app.use(logger('dev'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
@@ -14,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect("mongodb://localhost/workout",{
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/workout",{
     useNewUrlParser: true,
     useFindAndModify: false,
     useUnifiedTopology: true
@@ -26,3 +27,5 @@ app.use(require('./routes/api'));
 app.listen(PORT, () => {
         console.log(`App running on ${PORT}`);
 });
+
+
