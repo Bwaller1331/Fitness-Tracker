70af18e9047ccc604945c1e0a2f83ce509c3efde
Brandon Waller <bwall1331@gmail.com>
2021-06-16 09:39:55 -0400

    added exerciseSchema

--- models/exercises.js
+++ models/exercises.js
@@ -5,12 +5,33 @@ const exerciseSchema = new mongoose.Schema({
         type:Date,
         default: Date.now(),
 
-    }
-    exercises: [{
-        type: String,
-        trim:true,
-        required: 
+    },
+    exercises: [
+      { 
+        name: {
+            type:String,
+            trim:true,
+            required:[true,"Give your exercise a name"]
+            },
+        type:{
+            type: String,
+            trim:true,
+            required:[true, "Enter the exercise type"],
+            },
+        weights: Number,
 
-        
-    }]
-})
+        sets: Number,
+
+        reps: Number,
+
+        duration: {
+            type:Number,
+            required:[true,"Enter a workout duration in minutes"]
+        },
+        distance: Number
+
+       }
+    ],
+});
+
+const Exercise = mongoose.model('Exercise', exerciseSchema);
